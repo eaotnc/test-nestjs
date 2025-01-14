44aa70bf641879804159e3d80d2dc9b7112019ed
Eao Thanatcha <bonsai.eao@gmail.com>
2025-01-15 02:50:12 +0700

    add Todo entity and update TodoService to manage todos

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './todo.entity';
 
 @Injectable()
 export class TodoService {
+  todos: Todo[] = [];
   getTodos() {
     return [1, 2, 3];
   }
 
   createTodo(title: string, subtitle: string) {
+    const todo = new Todo();
+    todo.id = 1;
+    todo.title = title;
+    todo.subtitle = subtitle;
+    this.todos.push(todo);
     return `Create todo ${title}: ${subtitle}`;
   }
 
